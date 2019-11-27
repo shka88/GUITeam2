ff063d8d71b8c8d7095802c28e4fe670ae967ced
YuikaTeraoka <yuikasunmoon@gmail.com>
2019-11-27 19:18:27 +0900

    각 클래스에 이름과 학번을 넣았습니다.

--- LibraryListenenr.java
+++ LibraryListenenr.java
@@ -3,13 +3,14 @@ import java.awt.event.*;
 /**
  * 여기에 LibraryListenenr 클래스 설명을 작성하십시오.
  * 
- * @author () 
+ * @author (2018315056 우메모토 세이야, 2018315036 양유석, 2018315030 이가영, 2018315053 테라오카 유이카) 
  * @version (버전번호나 날짜)
  */
 public class LibraryListenenr implements ActionListener
 {
     JButton rbr,rbk,fl,ol,borrower,book,loan;
     JTextField txt; 
+    JList jl;
     public LibraryListenenr(){
         
         rbr.addActionListener(this);
